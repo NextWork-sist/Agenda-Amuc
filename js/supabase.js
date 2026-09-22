d26acd6364338a8e68537f12f208a7a79b23cded
NextWork-sist <nextworknqn@gmail.com>
2026-09-22 14:52:54 -0300

    Delete js directory

--- js/supabase.js
+++
@@ -1,7 +0,0 @@
-const SUPABASE_URL = 'https://vjwdekniufljzziozxog.supabase.co';
-const SUPABASE_PUBLISHABLE_KEY = 'sb_publishable_pkJK0Nv8tMMfPPDTHSuVXw_kXmXAdj2';
-
-const supabaseClient = window.supabase.createClient(
-  SUPABASE_URL,
-  SUPABASE_PUBLISHABLE_KEY
-);
